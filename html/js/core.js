0e761e87a521747e77868a5c3c1a6fe9cfcbbbe1
Aakash <akash.bhatia1184@gmail.com>
2016-03-31 23:25:16 +1100

    Products Grid

---
+++ html/js/core.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+$(document).ready(function() {
+    "use strict";
+	
+	$('.products-grid .product').mouseenter(function() {
+		$(this).find('.details').slideDown('fast');
+	});
+
+	$('.products-grid .product').mouseleave(function() {
+		$(this).find('.details').slideUp('fast');
+	});
+});
