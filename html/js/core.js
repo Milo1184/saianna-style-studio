bf8e33b865fa7060dadbd12701dc1ee65e3c5e61
Aakash Bhatia <aakash@stocksdigital.com>
2016-04-18 17:42:15 +1000

    Button Styles

--- html/js/core.js
+++ html/js/core.js
@@ -11,7 +11,25 @@ $(document).ready(function() {
 		$(this).find('.details').slideUp('fast');
 	});
 	
-  $(function() {
-    $(".rslides").responsiveSlides();
-  });
+	$(function() {
+		$(".rslides").responsiveSlides();
+	});
+	
+	$('.link-1').each(function() {
+        $(this).append(' <span>&#10095;</span>');
+    });
+	
+	$('.link-1').mouseenter(function() {
+		$(this).animate({
+			'padding-right': 10
+		}, 100);
+		$(this).find( 'span' ).toggle( "slide", {directon:"right"}, 300 );
+	});
+	
+	$('.link-1').mouseleave(function() {
+		$(this).animate({
+			'padding-right': 0
+		}, 100);
+		$(this).find( 'span' ).toggle( "slide", {directon:"right"}, 300 );
+	});	
 });
