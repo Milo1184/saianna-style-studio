3b9653b03ac10051c851a7f1488eda1a50961c75
Aakash <akash.bhatia1184@gmail.com>
2016-07-13 17:10:14 +1000

    default template

--- wp-content/themes/saianna/page.php
+++ wp-content/themes/saianna/page.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 	<main id="default-template" class="cms-content">
- 	<h1><?php the_title(); ?></h1>
-    
-    <?php the_content(); ?>
+    	<div class="wrap">
+            <h1><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+        <!-- div.wrap ENDS -->
+        </div>
     </main>
 <?php get_footer(); ?>
